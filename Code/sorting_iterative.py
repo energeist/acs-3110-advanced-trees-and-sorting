ab5d7bde9ce145d8ad2ca7680b43a2df04e08ecc
Mark Rattle <mark.rattle@gmail.com>
2023-10-22 14:58:00 -0400

    selection sort

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -63,14 +63,39 @@ def selection_sort(items):
     """Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    
+    Time complexity:
+    Worst case - O(n^2) - algorithm has to iterate over a double loop
+    Average case - O(n^2) - same a above
+    Best case - O(n^2) - algorithm must complete nested loop traversal regardless of whether the list
+    is sorted or not, since we are picking a minimum and comparing to each following value, then moving
+    to the next in the list and performing the same action until all values are compared. 
+    
+    TODO: Memory usage: ??? Why and under what conditions?
+    
+    Space complexity is always O(1), there are only small tracking variables stored.
+    """
+    
     # TODO: Repeat until all items are in sorted order
     # TODO: Find minimum item in unsorted items
     # TODO: Swap it with first unsorted item
 
     if len(items) in [0,1]: # early return for sorted cases
         return items
-
+    
+    for i in range(len(items)): # iterate over the list of items
+        min_index = i # initially assume the minimum value for the iteration is the first item chosen 
+        
+        for j in range(i+1, len(items)): # iterate over the remaining items
+        
+            if items[j] < items[min_index]: # if a smaller value is found
+                min_index = j # set that value to the minimum
+        
+        items[min_index], items[i] = items[i], items[min_index] 
+        # swap the smallest value found with the starting position of this iteration
+    
+    return items
+    
 def insertion_sort(items):
     """Sort given items by taking first unsorted item, inserting it in sorted
     order in front of items, and repeating until all items are in order.
@@ -97,8 +122,10 @@ def insertion_sort(items):
     for i in range(1, len(items)): # start counting from the second item in the list
         key = items[i] # pick a pivot point that is being sorted on
         j = i - 1 # start comparing against the previous item
+        
         while j >= 0 and key < items[j]: # if the pivot is smaller than the previous item then it must swap
             items[j + 1], items[j] = items[j], items[j + 1] # swap
             j -= 1 # move backwards through list to keep checking whether pivot item is in the right place
+    return items
             
     
